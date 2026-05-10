b175538b0d5ba4316d7467d9a2afa5d8e4bcd79b
waqarwasif <waqarwasif0333@gmail.com>
2026-05-10 10:08:12 +0500

    Boilerplate code reduced

--- src/PDFGenerator/PdfGenerator.h
+++ src/PDFGenerator/PdfGenerator.h
@@ -1,9 +1,5 @@
-#ifndef PDF_GENERATOR_H
-#define PDF_GENERATOR_H
-
+#pragma once
 #include <string>
 
 // Function to convert a string to a PDF
 void makePdf(std::string filename, std::string title, std::string body);
-
-#endif
